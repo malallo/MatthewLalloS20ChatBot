9cbc4350659e20294655f5e7e37005e219b69681
malallo <33700000+malallo@users.noreply.github.com>
2020-05-09 14:23:25 -0400

    Starting stuff

---
+++ server.py
@@ -0,0 +1,32 @@
+import http.server
+import socketserver
+from google_search import chatbot_query
+from google_search import greeting
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import os
+from TestBotServer import reply
+
+PORT = 8080
+DIRECTORY = 'public'
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=DIRECTORY, **kwargs)
+
+    def do_POST(self):
+        self.send_response(200)
+        content_length = int(self.headers['Content-Length'])
+        post_body = self.rfile.read(content_length)
+        self.end_headers()
+        print('user query', post_body)
+        google_search_chatbot_reply = reply(str(post_body))
+        self.wfile.write(str.encode(str(google_search_chatbot_reply)))
+
+with socketserver.TCPServer(('', PORT), Handler) as httpd:
+    print('serving at port', PORT)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
